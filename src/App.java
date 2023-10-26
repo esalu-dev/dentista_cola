4b97af1ef5d5abad612d708d4d5be7641acd4a81
esalu-dev <esalu88@outlook.com>
2023-10-25 21:01:38 -0600

    Updated pseudocode

--- src/App.java
+++ src/App.java
@@ -1,83 +1,116 @@
-import java.util.Scanner;
-
 public class App {
-    static int[] queue = new int[5];
-    static int front = -1;
-    static int rear = -1;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean exit = false;
-        System.out.println("Queues");
-        while(!exit){
-            System.out.println("------");
-            System.out.println("1. Enqueue");
-            System.out.println("2. Dequeue");
-            System.out.println("3. Exit");            
-            int choice = Integer.parseInt(sc.nextLine());
-            switch(choice){
-                case 1:
-                    System.out.println("Enter item to enqueue");
-                    int item = Integer.parseInt(sc.nextLine());
-                    enqueue(item);
-                    break;
-                case 2:
-                    dequeue();
-                    break;
-                default:
-                    exit = true;
-                    break;
-            }
-        }
-        sc.close();
-        
+    private int maxSize;
+    private int front;
+    private int rear;
+    private int[] queue;
+
+    public App(int size) {
+        maxSize = size;
+        front = -1;
+        rear = -1;
+        queue = new int[maxSize];
     }
-    public static void enqueue(int item) {
-        if (rear == queue.length - 1) {
-        if(front == 0){
-            System.out.println("Queue is full");
-            return;
-        }else {
-            rear = 0;
-            queue[rear] = item;
-            System.out.println("Inserted " + item);
-            showStatus();
-        }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+    public boolean isFull() {
+        return (front == 0 && rear == maxSize - 1) || (front == rear + 1);
+    }
+
+    public void enqueue(int data) {
+        if (isFull()) {
+            System.out.println("La cola está llena. No se puede agregar más elementos.");
         } else {
-            if(rear == front - 1){
-                System.out.println("Queue is full");
-                return;
-            }
             if (front == -1) {
                 front = 0;
             }
-            rear++;
-            queue[rear] = item;
-            System.out.println("Inserted " + item);
-            showStatus();
+            rear = (rear + 1) % maxSize;
+            queue[rear] = data;
+            System.out.println("Se agregó el elemento " + data + " a la cola.");
         }
     }
-    public static int dequeue() {
-        int item = -1;
-        if (front == -1) {
-            System.out.println("Queue is empty");
-        } else {
-            item = queue[front];
-            if (front % queue.length == rear) {
-                front = -1;
-                rear = -1;
-            } else {
-                front++;
-            }
-            System.out.println("Deleted " + item);
-            showStatus();
-        }
-        return item;
-    }
-    public static void showStatus(){
-        System.out.println("Front: " + front + " Rear: " + rear);
-        System.out.println("Queue: " + java.util.Arrays.toString(queue));
 
+    public int dequeue() {
+        int data;
+        if (isEmpty()) {
+            System.out.println("La cola está vacía. No se puede eliminar ningún elemento.");
+            return -1;
+        } else {
+            data = queue[front];
+            if (front == rear) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % maxSize;
+            }
+            System.out.println("Se eliminó el elemento " + data + " de la cola.");
+            return data;
+        }
+    }
+
+    
+    private static void printQueue(App queue) {
+        if (queue.isEmpty()) {
+            System.out.println("La cola está vacía.");
+        } else {
+            System.out.println("La cola contiene los siguientes elementos:");
+            if (queue.front <= queue.rear) {
+                for (int i = queue.front; i <= queue.rear; i++) {
+                    System.out.print(queue.queue[i]+", ");
+                }
+            } else {
+                for (int i = queue.front; i < queue.maxSize; i++) {
+                    System.out.print(queue.queue[i]+", ");
+                }
+                for (int i = 0; i <= queue.rear; i++) {
+                    System.out.print(queue.queue[i]+", ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        App queue = new App(5);
+
+        queue.enqueue(1);
+        printQueue(queue);
+        queue.enqueue(2);
+        printQueue(queue);
+        queue.enqueue(3); 
+        printQueue(queue);
+        queue.enqueue(4);
+        printQueue(queue);
+        queue.enqueue(5);
+        printQueue(queue);
+
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+
+        queue.enqueue(6);
+        printQueue(queue);
+        queue.enqueue(7);
+        printQueue(queue);
+        queue.enqueue(8);
+        printQueue(queue);
+        queue.enqueue(9);
+        printQueue(queue);
+        queue.enqueue(10);
+        printQueue(queue);
+
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+        System.out.println(queue.dequeue());
+        printQueue(queue);
+        System.out.println(queue.dequeue());
+        printQueue(queue);
     }
 }
-
-
